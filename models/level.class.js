914de95c381e228fb59356b080738b2815154dbe
Felix Pfeifer <felix@pr-media.at>
2026-06-06 18:31:49 +0200

    Add salsa parameter to Level constructor; enhance level initialization

--- models/level.class.js
+++ models/level.class.js
@@ -1,14 +1,16 @@
 class Level {
     clouds;
     coins;
+    salsa;
     enemies;
     backgroundObjects;
     level_end_x = 4800;
 
-    constructor(clouds, coins, enemies, backgroundObjects) {
+    constructor(clouds, coins,salsa, enemies, backgroundObjects) {
         this.clouds = clouds;
         this.coins = coins;
         this.enemies = enemies;
         this.backgroundObjects = backgroundObjects;
+        this.salsa = salsa;
     }
 }
