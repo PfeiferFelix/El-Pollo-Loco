746ea7e1df66dcfccc0481cab0a2e08392712ebf
Felix Pfeifer <felix@pr-media.at>
2026-06-08 20:06:31 +0200

    refactor: Rearrange image arrays and update loading logic in Endboss class

--- models/endboss.class.js
+++ models/endboss.class.js
@@ -2,7 +2,8 @@ class Endboss extends MovableObject {
     height = 400;
     width = 300;
     y = 50;
-    IMAGES_WALKING = [
+
+    IMAGES_ALERT = [
         "img/4_enemie_boss_chicken/2_alert/G5.png",
         "img/4_enemie_boss_chicken/2_alert/G6.png",
         "img/4_enemie_boss_chicken/2_alert/G7.png",
@@ -11,18 +12,40 @@ class Endboss extends MovableObject {
         "img/4_enemie_boss_chicken/2_alert/G10.png",
         "img/4_enemie_boss_chicken/2_alert/G11.png",
         "img/4_enemie_boss_chicken/2_alert/G12.png",
+    ]
+
+    IMAGES_WALKING = [
+        "img/4_enemie_boss_chicken/1_walk/G1.png",
+        "img/4_enemie_boss_chicken/1_walk/G2.png",
+        "img/4_enemie_boss_chicken/1_walk/G3.png",
+        "img/4_enemie_boss_chicken/1_walk/G4.png",
     ];
 
     constructor() {
-        super().loadImage(this.IMAGES_WALKING[0]);
+        super().loadImage("img/4_enemie_boss_chicken/2_alert/G5.png");
+        this.speed = 0.15
         this.loadImages(this.IMAGES_WALKING);
+        this.loadImages(this.IMAGES_ALERT);
         this.x = 4800;
         this.animate();
     }
 
     animate() {
-        setInterval(() => {
-            this.playAnimation(this.IMAGES_WALKING);
+         setInterval(() => {
+            if(this.character.x >= 3000){
+                this.playAnimation(this.IMAGES_WALKING);
+            }else {
+                this.playAnimation(this.IMAGES_ALERT);
+            }
         }, 200);
+         setInterval(() => {
+            if(this.character.x >= 3000){
+                this.moveLeft();
+            }
+        }, 1000 / 60);
     }
+
+
+
+    
 }
