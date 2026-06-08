e7450040748ae856113c9a6b89ed797e366734f2
Felix Pfeifer <felix@pr-media.at>
2026-06-08 20:06:41 +0200

    refactor: Update THROW_BOTTLE array and adjust throw method for improved animation

--- models/throwable-object.class.js
+++ models/throwable-object.class.js
@@ -1,20 +1,34 @@
 class ThrowableObject extends MovableObject{
+    THROW_BOTTLE = [
+        "img/6_salsa_bottle/bottle_rotation/1_bottle_rotation.png",
+        "img/6_salsa_bottle/bottle_rotation/2_bottle_rotation.png",
+        "img/6_salsa_bottle/bottle_rotation/3_bottle_rotation.png",
+        "img/6_salsa_bottle/bottle_rotation/4_bottle_rotation.png",
+    ];
+
     constructor(x, y){
         super().loadImage("img/6_salsa_bottle/salsa_bottle.png");
+        this.loadImages(this.THROW_BOTTLE);
         this.x = x;
         this.y = y;
-        this.throw();
         this.height = 100;
         this.width = 80;
-        
- 
+        this.throw();
+        this.animate();
    }
 
-  throw(){
-    this.speedY = 30;
-    this.applyGravity();
-    setInterval(() => {
-        this.x += 10;
-    }, 50);
-  }
+    animate(){
+        setInterval(() => {
+            this.playAnimation(this.THROW_BOTTLE);
+        }, 100);
+    }
+
+    throw(){
+        this.speedY = 5;
+        this.applyGravity();
+        setInterval(() => {
+            this.x += 60;
+        }, 60);
+    }
+
 }
