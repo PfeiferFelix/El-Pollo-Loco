90dcc795aaf0b2b1e0e4038b945fa1c2d716a15d
Felix Pfeifer <felix@pr-media.at>
2026-06-09 22:40:03 +0200

    feat: Enhance Endboss class with attack, hurt, and dead animations

--- models/endboss.class.js
+++ models/endboss.class.js
@@ -21,31 +21,74 @@ class Endboss extends MovableObject {
         "img/4_enemie_boss_chicken/1_walk/G4.png",
     ];
 
+    IMAGES_HURT = [
+
+        "img/4_enemie_boss_chicken/4_hurt/G21.png",
+        "img/4_enemie_boss_chicken/4_hurt/G22.png",
+        "img/4_enemie_boss_chicken/4_hurt/G23.png",
+    ];
+
+    IMAGES_DEAD = [
+        "img/4_enemie_boss_chicken/5_dead/G24.png",
+        "img/4_enemie_boss_chicken/5_dead/G25.png",
+        "img/4_enemie_boss_chicken/5_dead/G26.png",
+    ];
+
+    IMAGES_ATTACK = [
+        "img/4_enemie_boss_chicken/3_attack/G13.png",
+        "img/4_enemie_boss_chicken/3_attack/G14.png",
+        "img/4_enemie_boss_chicken/3_attack/G15.png",
+        "img/4_enemie_boss_chicken/3_attack/G16.png",
+        "img/4_enemie_boss_chicken/3_attack/G17.png",
+        "img/4_enemie_boss_chicken/3_attack/G18.png",
+        "img/4_enemie_boss_chicken/3_attack/G19.png",
+        "img/4_enemie_boss_chicken/3_attack/G20.png",
+    ];
+
+
     constructor() {
         super().loadImage("img/4_enemie_boss_chicken/2_alert/G5.png");
         this.speed = 0.15
         this.loadImages(this.IMAGES_WALKING);
         this.loadImages(this.IMAGES_ALERT);
+        this.loadImages(this.IMAGES_HURT);
+        this.loadImages(this.IMAGES_DEAD);
         this.x = 4800;
         this.animate();
     }
 
+    hit() {
+        this.energy -= 20;
+        if (this.energy < 0) {
+            this.energy = 0;
+        } else {
+            this.lastHit = new Date().getTime();
+        }
+    }
+
     animate() {
-         setInterval(() => {
-            if(this.character.x >= 3000){
+        let animationInterval = setInterval(() => {
+            if (this.isHurt()) {
+                this.playAnimation(this.IMAGES_HURT);
+            } else if (this.isDead()) {
+                this.playAnimation(this.IMAGES_DEAD);
+                this.y = 80;
+                clearInterval(animationInterval);  //  Animation stoppen
+                clearInterval(moveInterval); // move left animation stoppen
+                return;
+            } else if (this.character.x >= 3000) {
                 this.playAnimation(this.IMAGES_WALKING);
-            }else {
+            } else {
                 this.playAnimation(this.IMAGES_ALERT);
             }
         }, 200);
-         setInterval(() => {
-            if(this.character.x >= 3000){
+        let moveInterval = setInterval(() => {
+            if (!this.character) return;
+            if (this.character.x >= 3000) {
                 this.moveLeft();
             }
         }, 1000 / 60);
     }
 
 
-
-    
 }
