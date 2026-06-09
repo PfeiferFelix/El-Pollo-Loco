bb30241bccac864a59cf622b12e7e33aaffa56fa
Felix Pfeifer <felix@pr-media.at>
2026-06-09 22:39:44 +0200

    feat: Add dead animation images and integrate into Chicken class behavior

--- models/chicken.class.js
+++ models/chicken.class.js
@@ -7,6 +7,9 @@ class Chicken extends MovableObject {
         "img/3_enemies_chicken/chicken_normal/1_walk/2_w.png",
         "img/3_enemies_chicken/chicken_normal/1_walk/3_w.png",
     ];
+    IMAGES_DEAD = [
+        "img/3_enemies_chicken/chicken_normal/2_dead/dead.png",
+    ];
     constructor() {
         super().loadImage(
             "img/3_enemies_chicken/chicken_normal/1_walk/1_w.png",
@@ -14,16 +17,29 @@ class Chicken extends MovableObject {
         this.x = 300 + Math.random() * 4800; // Zufällige Position zwischen 200 und 700
         this.speed = 0.15 + Math.random() * 0.5; // Zufällige Geschwindigkeit zwischen 0.15 und 0.65
         this.loadImages(this.IMAGES_WALKING);
+        this.loadImages(this.IMAGES_DEAD);
         this.animate();
+        
+    }
+
+    hit() {
+        this.energy = 0;
     }
 
     animate() {
-        setInterval(() => {
+        let animationInterval = setInterval(() => {
             this.moveLeft();
         }, 1000 / 60);
 
         setInterval(() => {
-            this.playAnimation(this.IMAGES_WALKING);
+            
+            if (this.isDead()) {
+                this.playAnimation(this.IMAGES_DEAD);
+                this.y = 335;
+                clearInterval(animationInterval);
+                return;
+            }else{this.playAnimation(this.IMAGES_WALKING);
+            }
         }, 100);
     }
 }
