3e0029bd8bd565803008aea3f12d0351d12f58d6
Felix Pfeifer <felix@pr-media.at>
2026-06-09 22:39:29 +0200

    feat: Add ChickenSmall class with walking and dead animations

---
+++ models/chicken-small.class.js
@@ -0,0 +1,44 @@
+class ChickenSmall extends MovableObject {
+    height = 100;
+    width = 90;
+    y = 330;
+    IMAGES_WALKING = [
+        "img/3_enemies_chicken/chicken_small/1_walk/1_w.png",
+        "img/3_enemies_chicken/chicken_small/1_walk/2_w.png",
+        "img/3_enemies_chicken/chicken_small/1_walk/3_w.png",
+    ];
+    IMAGES_DEAD = [
+        "img/3_enemies_chicken/chicken_small/2_dead/dead.png",
+    ];
+    constructor() {
+        super().loadImage(
+            "img/3_enemies_chicken/chicken_small/1_walk/1_w.png",
+        );
+        this.x = 300 + Math.random() * 4800; // Zufällige Position zwischen 200 und 700
+        this.speed = 0.15 + Math.random() * 0.5; // Zufällige Geschwindigkeit zwischen 0.15 und 0.65
+        this.loadImages(this.IMAGES_WALKING);
+        this.loadImages(this.IMAGES_DEAD);
+        this.animate();
+    }
+
+      hit() {
+        this.energy = 0;
+    }
+
+    animate() {
+        let animationInterval = setInterval(() => {
+            this.moveLeft();
+        }, 1000 / 60);
+
+        setInterval(() => {
+            
+            if (this.isDead()) {
+                this.playAnimation(this.IMAGES_DEAD);
+                this.y = 330;
+                clearInterval(animationInterval);
+                return;
+            }else{this.playAnimation(this.IMAGES_WALKING);
+            }
+        }, 100);
+    }
+}
