783fe6389a606b475a30e15c0640c77224eb8293
Felix Pfeifer <felix@pr-media.at>
2026-06-09 22:39:54 +0200

    feat: Include ChickenSmall in drawFrame condition for proper rendering

--- models/drawable-object.js
+++ models/drawable-object.js
@@ -34,7 +34,7 @@ img;
         });
     }
        drawFrame(ctx){
-        if(this instanceof Character || this instanceof Chicken || this instanceof SalsaBottle || this instanceof Endboss){
+        if(this instanceof Character || this instanceof Chicken || this instanceof ChickenSmall || this instanceof SalsaBottle || this instanceof Endboss){
          ctx.beginPath();
         ctx.lineWidth = "5";
         ctx.strokeStyle = "blue";
