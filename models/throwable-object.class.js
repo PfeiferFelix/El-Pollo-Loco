79c19cd94f78d54c9e29d5611f05deb079b56d86
Felix Pfeifer <felix@pr-media.at>
2026-06-09 22:40:19 +0200

    feat: Update ThrowableObject class with improved throw mechanics and added BOTTLE_SPLASH images

--- models/throwable-object.class.js
+++ models/throwable-object.class.js
@@ -1,34 +1,51 @@
-class ThrowableObject extends MovableObject{
-    THROW_BOTTLE = [
-        "img/6_salsa_bottle/bottle_rotation/1_bottle_rotation.png",
-        "img/6_salsa_bottle/bottle_rotation/2_bottle_rotation.png",
-        "img/6_salsa_bottle/bottle_rotation/3_bottle_rotation.png",
-        "img/6_salsa_bottle/bottle_rotation/4_bottle_rotation.png",
-    ];
+class ThrowableObject extends MovableObject {
+  THROW_BOTTLE = [
+    "img/6_salsa_bottle/bottle_rotation/1_bottle_rotation.png",
+    "img/6_salsa_bottle/bottle_rotation/2_bottle_rotation.png",
+    "img/6_salsa_bottle/bottle_rotation/3_bottle_rotation.png",
+    "img/6_salsa_bottle/bottle_rotation/4_bottle_rotation.png",
 
-    constructor(x, y){
-        super().loadImage("img/6_salsa_bottle/salsa_bottle.png");
-        this.loadImages(this.THROW_BOTTLE);
-        this.x = x;
-        this.y = y;
-        this.height = 100;
-        this.width = 80;
-        this.throw();
-        this.animate();
-   }
+  ];
 
-    animate(){
-        setInterval(() => {
-            this.playAnimation(this.THROW_BOTTLE);
-        }, 100);
-    }
+  BOTTLE_SPLASH = [
+    "img/6_salsa_bottle/bottle_rotation/bottle_splash/1_bottle_splash.png",
+    "img/6_salsa_bottle/bottle_rotation/bottle_splash/2_bottle_splash.png",
+    "img/6_salsa_bottle/bottle_rotation/bottle_splash/3_bottle_splash.png",
+    "img/6_salsa_bottle/bottle_rotation/bottle_splash/4_bottle_splash.png",
+    "img/6_salsa_bottle/bottle_rotation/bottle_splash/5_bottle_splash.png",
+    "img/6_salsa_bottle/bottle_rotation/bottle_splash/6_bottle_splash.png",
+  ];
 
-    throw(){
-        this.speedY = 5;
-        this.applyGravity();
-        setInterval(() => {
-            this.x += 60;
-        }, 60);
-    }
+  constructor(x, y) {
+    super().loadImage("img/6_salsa_bottle/salsa_bottle.png");
+    this.loadImages(this.THROW_BOTTLE);
+    this.x = x;
+    this.y = y;
+    this.height = 100;
+    this.width = 80;
+    this.throw();
+
+  }
+
+  animate() {
+    setInterval(() => {
+      this.playAnimation(this.THROW_BOTTLE);
+    }, 100);
+  }
+
+  throw() {
+    this.speedY = 10; //Bogen desto größer derso höher der Bogen der die Flasche fliegt
+    this.applyGravity();
+    let distance = 0; //Startwert
+    let ThrowIntervall = setInterval(() => {
+      this.x += 10; //geschwindigkeit dersto höher desto schneller nach rechts
+      distance += 5;
+      if (distance >= 500) { //Wie weit die Flasche fliegt
+        clearInterval(ThrowIntervall);
+      }
+    }, 25);
+    this.animate();
+  }
+
 
 }
