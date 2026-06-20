a2bc694d6df220e4fcccb5808f2a0572379eb2ae
Felix Pfeifer <felix@pr-media.at>
2026-06-20 17:56:31 +0200

    feat: Add sound mute check before playing audio in World class

--- models/world.class.js
+++ models/world.class.js
@@ -74,7 +74,7 @@ class World {
                 if (endboss.isColliding(bottle)) {
                     endboss.hit();
                     this.statusBarBoss.setPercantage(endboss.energy);
-                    this.audio_splash_bottle.play();
+                    if (!soundsMuted) this.audio_splash_bottle.play();
                     return false;
                 }
                 return true;
@@ -85,7 +85,7 @@ class World {
                 this.throwableObject = this.throwableObject.filter((bottle) => {
                     if (enemy.isColliding(bottle) && !enemy.isDead()) {
                         enemy.hit();
-                        this.audio_splash_bottle.play();
+                        if (!soundsMuted) this.audio_splash_bottle.play();
                         return false;
                         
                     }
@@ -105,7 +105,7 @@ class World {
             if (this.character.isColliding(coin)) {
                 this.level.coins.splice(index, 1)
                 this.audio_collect_coin.currentTime = 0;
-                this.audio_collect_coin.play();
+                if (!soundsMuted) this.audio_collect_coin.play();
                 if (this.coins < 5) {
                     this.coins++;
                 }
