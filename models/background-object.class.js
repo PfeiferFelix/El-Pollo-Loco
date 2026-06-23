28ac0349ad1d3c16480f08ccb4fda3f39ce6c6e4
Felix Pfeifer <felix@pr-media.at>
2026-06-23 22:47:42 +0200

    refactor: Improve documentation for BackgroundObject class and constructor parameters

--- models/background-object.class.js
+++ models/background-object.class.js
@@ -1,7 +1,21 @@
+/**
+ * Represents a static background tile in the game world.
+ * Background objects fill the full canvas height and are positioned
+ * along the x-axis to compose the scrolling level scenery.
+ * @extends MovableObject
+ */
 class BackgroundObject extends MovableObject {
+    /** @type {number} Fixed height matching the canvas height in pixels. */
     height = 480;
+
+    /** @type {number} Fixed width matching the canvas width in pixels. */
     width = 720;
 
+    /**
+     * Creates a new BackgroundObject.
+     * @param {string} imagePath - Path to the background image asset.
+     * @param {number} x - Horizontal position of the object in the game world.
+     */
     constructor(imagePath, x) {
         {
             super().loadImage(imagePath);
