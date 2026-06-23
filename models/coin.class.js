a50d2d9891346fb84c2ecaabb3b376ff4ed07898
Felix Pfeifer <felix@pr-media.at>
2026-06-23 22:48:19 +0200

    fix: Correct coin initial position for proper spawning in the game

--- models/coin.class.js
+++ models/coin.class.js
@@ -5,8 +5,8 @@ class Coin extends MovableObject{
 
      constructor() {
         super().loadImage("img/8_coin/coin_1.png");
-        this.x = 0 + Math.random() * 3800; // Zufällige Position zwischen 200 und 700 auf der X achse
-        this.y = 200 + Math.random() * 100
+        this.x = 1000 + Math.random() * 3800;
+        this.y = 200 + Math.random() * 100;
         
     }
 }
